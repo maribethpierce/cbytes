028dce220dc1160364cb95d6019e5321a7917842
Maribeth Pierce <maribeth.pierce@hellolabs.com>
2016-05-17 13:49:00 -0400

    made it a class

--- arrayflattener.rb
+++ arrayflattener.rb
@@ -1,17 +1,30 @@
 require 'pry'
 this = [[1,2,[3]],4]
+that = ["joe", ["del", "bart"], ["hank", "jimm", "pete"], "perl"]
+class ArraySmasher
+  attr_reader :results, :array
 
-@result = []
-def smash(arr)
-  arr.each do |a|
-    # binding.pry
-    if a.class != Array
-      @result << a
-    else
-      smash(a)
-    end
+  def initialize(array)
+    @array = array
   end
-  @result
-end
 
-print smash(this)
+  @@results = Array.new
+  def smash
+    self.array.each do |a|
+      unless a == nil
+        if a.class != Array
+          @@results << a
+        else
+          temp_array_smasher = ArraySmasher.new(a)
+          temp_array_smasher.smash
+        end
+      end
+    end
+    @@results
+  end
+end
+that = ArraySmasher.new(that)
+this = ArraySmasher.new(this)
+print this.smash
+print "\n"
+print that.smash
